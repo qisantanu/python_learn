58e36c816d035af526badc0cb6c034572577a12c
Santanu Bhattachrya <santanu.bhattacharya@quantuminventions.com>
2025-08-09 19:02:48 +0530

    learn: initial commit for python

---
+++ data_type_python.py
@@ -0,0 +1,16 @@
+name = "Alice"
+age = 30
+
+# This will print: "Alice is 30 years old."
+print(name + " is " + str(age) + " years old.")
+
+print(f"{name} is {age} years old.")
+
+x = 15
+y = 5
+z = 4
+
+print(x/y)  # This will print: 3.0
+print(x//y)  # This will print: 3
+print(x % y)  # This will print: 0
+print(x/z)  # This will print: 3.75
---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello world")
+
+# A variable named 'message' is created and assigned a string value
+message = "Hello, Python!"
+
+# Now, we can use the variable to print the message
+print(message)
